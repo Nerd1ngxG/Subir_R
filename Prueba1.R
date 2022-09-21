ac8b4a4cf1c9a9609de5b0a49bf2da18863212a8
blcr342 <38838613+blcr342@users.noreply.github.com>
2022-09-21 14:25:51 +0200

    Primera adición

---
+++ Prueba1.R
@@ -0,0 +1,13 @@
+print("hlelo")
+
+
+data(mtcars)
+#install.packages("data.table",
+#                 repos="http://R-Forge.R-project.org")
+library(data.table)
+
+#Para Importar datos de un fichero utilizar fread
+mtcars <- as.data.table(mtcars)
+
+#Filtrar datos
+mtcars[cyl==6]
